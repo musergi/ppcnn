c3cef6e340dec10f3fab03ac725052c1e833c32f
Norma Gutiérrez <normagutierrez@10-192-42-102client.eduroam.upc.edu>
2020-02-17 12:58:00 +0100

    refactor

--- ppcnn/__main__.py
+++ ppcnn/__main__.py
@@ -3,7 +3,12 @@ from ppcnn import client
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--server', help='run server instead of client')
+    parser.add_argument('-s', '--server', action='store_true', help='run server instead of client')
+    parser.add_argument('-a', '--address', type=str, help='specify server address to client')
+    parser.add_argument('-t', '--target', type=str,help='target data to train on')
     args = parser.parse_args()
 
-    client.run()
+    if args.server:
+        print('Running server')
+    else:
+        client.run()
--- ppcnn/client.py
+++ ppcnn/client.py
@@ -4,16 +4,23 @@ from ppcnn import coms
 import tensorflow as tf
 
 
-def run():
-    clientsocket = socket.socket()
-    clientsocket.connect((socket.gethostname(), coms.PORT))
-    sock = coms.CustomSocket(clientsocket)
+def get_model(sock):
     sock.send_str('GETNET')
     net_json = sock.recv_str()
     model = tf.keras.models.model_from_json(net_json)
     model.summary()
     weights = pickle.loads(sock.recv_data())
     model.set_weights(weights)
+    return model
+
+
+
+def run():
+    clientsocket = socket.socket()
+    clientsocket.connect((socket.gethostname(), coms.PORT))
+    sock = coms.CustomSocket(clientsocket)
+    
+    model = get_model(sock)
     # Train network
     sock.send_data(pickle.dumps(model.get_weights()))
     if sock.recv_str() == 'OK':
