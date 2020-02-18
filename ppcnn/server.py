c046945e76fcfd46eabb11af1bc0c053b7aa2507
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-02-18 15:10:00 +0100

    fumada muy heavy

--- ppcnn/server.py
+++ ppcnn/server.py
@@ -3,16 +3,25 @@ import threading
 import pickle
 from ppcnn import nets, coms
 
+weights = nets.Controller().get_weights()
 
 def on_connection(clientsocket):
     sock = coms.CustomSocket(clientsocket)
     if sock.recv_str() == 'GETNET':
         sock.send_str(nets.Controller().to_json())
-        sock.send_data(pickle.dumps(nets.Controller().get_weights())) # Serialize and send weight data
+        sock.send_data(pickle.dumps(weights)) # Serialize and send weight data
         new_weights = pickle.loads(sock.recv_data())
         sock.send_str('OK')
     elif sock.recv_str() == 'VALNET':
         print('Load validation dataset and print result')
+    return new_weights
+
+#No se com fer això: el que vull es actualitzar correctament els pesos
+def update_weights (new_weights):
+    for delta_weights in weights:
+        delta_weights -= new_weights
+        return delta_weights
+
 
 
 def run():
