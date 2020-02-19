da1e39e4c6f6ebfc87cb27f6b9a8d1bb8e663ca4
musergi <sergi.mus@gmail.com>
2020-02-19 18:38:08 +0100

    Started run file

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH --chdir=/scratch/nas/4/norma/ppcnn
+#SBATCH --job-name="ppcnn"
+#SBATCH --output=/scratch/nas/4/norma/.log/stdout-%j.out
+#SBATCH --error=/scratch/nas/4/norma/.log/stderr-%j.out
+#SBATCH --wait-all-nodes=1
+
+# Paths
+PYTHON=/scratch/nas/4/norma/venv/bin/python
+SERVER_INIT_FILE=.sync/server_ready.out
+
+# Search for host node and start server on it
+for node in `scontrol show hostnames $SLURM_JOB_NODELIST`; do
+    if ["$HOSTNAME" = "$node"] ; then
+        $PYTHON -m ppcnn --server &
+    fi
+done
+
+# Save server process id
+SERVER_JOB_PID=`jobs -p`
+
+# Wait for server to initialize
+SERVER_INIT = 0
+while [$SERVER_INIT -e 0]
+do
+    if test -f "$SERVER_INIT_FILE"; then
+        $SERVER_INIT = 1
+    fi
+done
+
+# Read address from file
+SERVER_ADDRESS = `cat $SERVER_INIT_FILE`
+
