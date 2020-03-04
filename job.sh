79431ae248c17f69a8eaa4814382baefa052629d
Norma Gutiérrez <normagutierrez@10-192-64-158client.eduroam.upc.edu>
2020-03-04 12:25:17 +0100

    Hotfix

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --chdir=/scratch/nas/4/norma/sequential_ppcnn
+#SBATCH --chdir=/scratch/nas/4/norma/ppcnn/sequential_ppcnn
 #SBATCH --output=/scratch/nas/4/norma/.log/stdout-%j.out
 #SBATCH --error=/scratch/nas/4/norma/.log/stderr-%j.out
 PYTHON="/scratch/nas/4/norma/venv/bin/python"
