34e4a73a54206984d5e2166ccad671dcda5eaa21
musergi <sergi.mus@gmail.com>
2020-03-07 16:29:41 +0100

    Sergi probando

--- job.sh
+++ job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --chdir=/scratch/nas/4/norma/ppcnn/sequential_ppcnn
+#SBATCH --chdir=/scratch/nas/4/norma/ppcnn
 #SBATCH --output=/scratch/nas/4/norma/.log/stdout-%j.out
 #SBATCH --error=/scratch/nas/4/norma/.log/stderr-%j.out
 PYTHON="/scratch/nas/4/norma/venv/bin/python"
 
-$PYTHON seq.py
+$PYTHON sequential_ppcnn/seq.py
