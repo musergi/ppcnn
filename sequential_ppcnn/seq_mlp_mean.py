8a3f04ac96df537f44902720358d2c868cc187ba
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-03-10 21:13:37 +0100

    Hotfix

--- sequential_ppcnn/seq_mlp_mean.py
+++ sequential_ppcnn/seq_mlp_mean.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
             
             # Load data
             print("Loading data")
-            x_train, y_train = load_data('datasets/split15/datasplit%04d.pickle' % i)
+            x_train, y_train = load_data('datasets/split5/datasplit%04d.pickle' % (i%5))
 
             # Train network
             print("training network")
