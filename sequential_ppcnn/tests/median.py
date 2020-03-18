40ff77073bd6ccee7eb06dccb13a59ca2e668959
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-03-18 16:43:21 +0100

    Refactoring

---
+++ sequential_ppcnn/tests/median.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def get_median_delta(deltas):
+    median_deltas = []
+    deltas_count = len(deltas)     # Numero de deltas de distintos nodos
+    layer_count = len(deltas[0])  # Numero de capas
+    for layer_index in range(layer_count):
+        layer_median_delta = deltas[0][layer_index]
+        for node_index in range(1, deltas_count):
+            layer_median_delta = np.median(layer_median_delta, axis=deltas[node_index][layer_index])
+        median_deltas.append(layer_median_delta)
+    return median_deltas
+
+deltas_node1 = [
+    np.array([[1, 1, 1], [1, 2, 1]]), # Layer 1
+    np.array([[2, 2, 2], [2, 7, 2]])  # Layer 2
+]
+deltas_node2 = [
+    np.array([[1, 4, 2], [1, 2, 3]]), # Layer 1
+    np.array([[2, 2, 2], [2, 4, 2]])  # Layer 2
+]
+deltas_node3 = [
+    np.array([[4, 2, 1], [3, 2, 1]]), # Layer 1
+    np.array([[5, 2, 2], [3, 7, 2]])  # Layer 2
+]
+deltas = [deltas_node1, deltas_node2, deltas_node3]
+
+median_delta = get_median_delta(deltas)
+
+print(median_delta[0])
+print(median_delta[1])
