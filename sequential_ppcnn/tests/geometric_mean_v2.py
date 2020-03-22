f967141065c908bb6bcc7686e4a3354d815e9bde
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-03-22 19:10:06 +0100

    Hotfix

--- sequential_ppcnn/tests/geometric_mean_v2.py
+++ sequential_ppcnn/tests/geometric_mean_v2.py
@@ -2,14 +2,35 @@ import numpy as np
  
 def get_geometric_mean_delta(deltas, nodes):
     gmean_deltas = []
-        for layer in range(len(deltas[0])):
-            gmean_layer_deltas = None
-            for node_deltas in deltas:
-                if gmean_layer_deltas is None:
-                    gmean_layer_deltas = node_deltas[layer] / len(deltas)
-                else:
-                    gmean_layer_deltas = gmean_layer_deltas * (node_deltas[layer] / len(deltas))
-            gmean_layer_deltas = np.sqrt(gmean_layer_deltas, order=nodes)
-            gmean_deltas.append(gmean_layer_deltas)
+    for layer in range(len(deltas[0])):
+        gmean_layer_deltas = None
+        for node_deltas in deltas:
+            if gmean_layer_deltas is None:
+                gmean_layer_deltas = node_deltas[layer] / len(deltas)
+            else:
+                gmean_layer_deltas = gmean_layer_deltas * (node_deltas[layer] / len(deltas))
+        gmean_layer_deltas = np.sqrt(gmean_layer_deltas, order=nodes)
+        gmean_deltas.append(gmean_layer_deltas)
     return gmean_deltas 
+
+
+deltas_node1 = [
+    np.array([[1, 1, 1], [1, 2, 1]]), # Layer 1
+    np.array([[2, 2, 2], [2, 7, 2]])  # Layer 2
+]
+deltas_node2 = [
+    np.array([[1, 4, 2], [1, 2, 3]]), # Layer 1
+    np.array([[2, 2, 2], [2, 4, 2]])  # Layer 2
+]
+deltas_node3 = [
+    np.array([[4, 2, 1], [3, 2, 1]]), # Layer 1
+    np.array([[5, 2, 2], [3, 7, 2]])  # Layer 2
+]
+deltas = [deltas_node1, deltas_node2, deltas_node3]
+
+geometric_mean_delta = get_geometric_mean_delta(deltas, 3)
+
+print(geometric_mean_delta[0])
+print(geometric_mean_delta[1])
+
  
