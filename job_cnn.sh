8da19d2f1281e85725c10905d09679e2ca7cbf43
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-03-27 09:10:37 +0100

    Created median

--- job_cnn.sh
+++ job_cnn.sh
@@ -5,4 +5,4 @@
 #SBATCH --error=/scratch/nas/4/norma/.log/stderr-%j.out
 PYTHON="/scratch/nas/4/norma/venv/bin/python"
 
-$PYTHON sequential_ppcnn/seq_cnn_gmean.py
+$PYTHON sequential_ppcnn/seq_cnn_median.py
--- job_mlp.sh
+++ job_mlp.sh
@@ -5,4 +5,4 @@
 #SBATCH --error=/scratch/nas/4/norma/.log/stderr-%j.out
 PYTHON="/scratch/nas/4/norma/venv/bin/python"
 
-$PYTHON sequential_ppcnn/seq_mlp_gmean.py
+$PYTHON sequential_ppcnn/seq_mlp_median.py
