a6739e3200f66e7b81ed55bbe2b45eb1f7ca2c9c
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-03-30 09:08:22 +0200

    Cagada pastoret

--- job_median.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --chdir=/scratch/nas/4/norma/ppcnn
-#SBATCH --output=/scratch/nas/4/norma/.log/stdout-%j.out
-#SBATCH --error=/scratch/nas/4/norma/.log/stderr-%j.out
-PYTHON="/scratch/nas/4/norma/venv/bin/python"
-
-$PYTHON sequential_ppcnn/tests/median.py
