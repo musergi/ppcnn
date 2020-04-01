60af46357b15c63aaf5e68ebde531cf1f8afe2e8
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-04-01 16:12:56 +0200

    Changed CNN 3 nodes and MLP 7 nodes

--- sequential_ppcnn/gaussianNoise/seq_cnn_mean.py
+++ sequential_ppcnn/gaussianNoise/seq_cnn_mean.py
@@ -2,7 +2,7 @@ import pickle
 import tensorflow as tf
 
 
-NODES = 5
+NODES = 3
 ITERATIONS = 20
 EPOCHS = 1
 MODEL_SAVE_PATH = 'temp_cnn.h5'
@@ -49,7 +49,7 @@ if __name__ == "__main__":
             
             # Load data
             print("Loading data")
-            x_train, y_train = load_data('datasets/split5/datasplit%04d.pickle' % i)
+            x_train, y_train = load_data('datasets/split3/datasplit%04d.pickle' % i)
 
             # Train network
             print("training network")
