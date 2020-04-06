f1d51d592d35732617cf68718d826eaaa98681a2
Norma-99 <normaescg@gmail.com>
2020-04-06 15:46:39 +0200

    Started implementing median

--- sequential_ppcnn/tests/median.py
+++ sequential_ppcnn/tests/median.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 def get_median_delta(deltas):
-    return np.median(deltas, axis=0)
+    median_deltas = []
+    deltas_count = len(deltas)     # Numero de deltas de distintos nodos
+    layer_count = len(deltas[0])  # Numero de capas
+    for layer_index in range(layer_count):
+        layer_ordered_delta = deltas[0][layer_index]
+        median_deltas[layer_index] = layer_ordered_delta.sort() / 2
+    return median_deltas(deltas.len()/2)
 
 deltas_node1 = [
     np.array([[1, 1, 1], [1, 2, 1]]), # Layer 1
