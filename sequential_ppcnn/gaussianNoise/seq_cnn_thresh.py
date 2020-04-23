8e5302b2d164d2c13650588a58d594607bf91959
Norma-99 <normaescg@gmail.com>
2020-04-23 20:48:11 +0200

    Hotfix

--- sequential_ppcnn/gaussianNoise/seq_cnn_thresh.py
+++ sequential_ppcnn/gaussianNoise/seq_cnn_thresh.py
@@ -1,8 +1,9 @@
 import pickle
 import tensorflow as tf
 import numpy as np
+import random
 
-NODES = 5
+NODES = 3
 ITERATIONS = 20
 EPOCHS = 1
 MODEL_SAVE_PATH = 'temp.h5'
@@ -18,39 +19,21 @@ def save_gradient(weights, gradient):
         iteration_deltas.append(final_layer_weights - inital_layer_weights)
     return iteration_deltas
 
-def get_median_delta(deltas):
+def get_thresh_delta(delta):
     median_deltas = []
-    
+    max_deltas = []
+    thresh_deltas = []
     nodes = range(0, len(deltas))
     layers = range(0, len(deltas[0]))
     for layer in layers:
         layer_weights = list()
+        layer_max_delta = deltas[0][layer]
         for node in nodes:
             layer_weights.append(deltas[node][layer])
-        median_deltas.append(np.median(layer_weights, axis=0))
-    return median_deltas
+            layer_max_delta = np.maximum(layer_max_delta, deltas[node][layer])
+        thresh_deltas.append(random.uniform(np.median(layer_weights, axis=0), layer_max_delta))
+    return thresh_deltas
 
-def get_max_delta(deltas):
-    max_deltas = []
-    deltas_count = len(deltas)     # Numero de deltas de distintos nodos
-    layer_count = len(deltas[0])  # Numero de capas
-    for layer_index in range(layer_count):
-        layer_max_delta = deltas[0][layer_index]
-        for node_index in range(1, deltas_count):
-            layer_max_delta = np.maximum(layer_max_delta, deltas[node_index][layer_index])
-        max_deltas.append(layer_max_delta)
-    return max_deltas
-
-def get_min_delta(deltas):
-    min_deltas = []
-    deltas_count = len(deltas)     # Numero de deltas de distintos nodos
-    layer_count = len(deltas[0])  # Numero de capas
-    for layer_index in range(layer_count):
-        layer_min_delta = deltas[0][layer_index]
-        for node_index in range(1, deltas_count):
-            layer_min_delta = np.minimum(layer_min_delta, deltas[node_index][layer_index])
-        min_deltas.append(layer_min_delta)
-    return min_deltas
 
 if __name__ == "__main__":
     # Create network
@@ -83,7 +66,7 @@ if __name__ == "__main__":
             
             # Load data
             print("Loading data")
-            x_train, y_train = load_data('datasets/split5/datasplit%04d.pickle' % i)
+            x_train, y_train = load_data('datasets/split3/datasplit%04d.pickle' % i)
 
             # Train network
             print("training network")
@@ -94,20 +77,14 @@ if __name__ == "__main__":
             iteration_deltas = save_gradient(initial_weights, final_weights)
             deltas.append(iteration_deltas)
 
-        # Calculate gradient max
-        max_delta = get_max_delta(deltas)
-        #Calculate gradient median
-        median_delta = get_median_delta(deltas)
-        #Calculate gradient min
-        min_delta = get_min_delta(deltas)
 
         #Define threshold and apply it
-        thres_delta = None
+        thresh_delta = get_thresh_delta
 
         # Apply deltas
         model = tf.keras.models.load_model(MODEL_SAVE_PATH)
         new_weights = []
-        for layer_weights, layer_deltas in zip(model.get_weights(), median_delta):
+        for layer_weights, layer_deltas in zip(model.get_weights(), thresh_delta):
             new_weights.append(layer_weights + layer_deltas)
         model.set_weights(new_weights)
 
