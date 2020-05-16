9edb1f756cebe8bbf7a6e42ac86a5f6724ff5b95
Norma-99 <normaescg@gmail.com>
2020-05-16 12:43:36 +0200

    Changed to 7 fog nodes

--- sequential_ppcnn/gaussianNoise/seq_cnn_mean.py
+++ sequential_ppcnn/gaussianNoise/seq_cnn_mean.py
@@ -2,7 +2,7 @@ import pickle
 import tensorflow as tf
 
 
-NODES = 5
+NODES = 7
 ITERATIONS = 20
 EPOCHS = 1
 MODEL_SAVE_PATH = 'temp_cnn.h5'
@@ -50,7 +50,7 @@ if __name__ == "__main__":
             
             # Load data
             print("Loading data")
-            x_train, y_train = load_data('datasets/split5/datasplit%04d.pickle' % i)
+            x_train, y_train = load_data('datasets/split7/datasplit%04d.pickle' % i)
 
             # Train network
             print("training network")
